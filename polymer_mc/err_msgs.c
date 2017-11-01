58869f457fe853cb17269439ea9c6bb8d2ba4583
kohnakagawa <tsunekou1019@gmail.com>
2017-11-02 01:52:59 +0900

    ディレクトリ構成変更

--- polymer_mc/err_msgs.c
+++
@@ -1,5 +0,0 @@
-#include "err_msgs.h"
-
-const char err_msgs[NUM_OF_ERR_CODE][128] = {
-  "Out of Range",
-};
--- polymer_mc/math_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATH_UTILS_H
-#define MATH_UTILS_H
-
-#include <stdint.h>
-#include <stdbool.h>
-
-bool isSquareNumber(const int32_t a);
-
-#endif
