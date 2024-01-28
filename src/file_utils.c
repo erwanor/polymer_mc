bacd6090ef040ba7c5ea7a4b61d2f5894ff37e4b
Erwan <erwanor@penumbralabs.xyz>
2024-01-28 13:29:51 -0500

    pmc: dump errors

--- src/file_utils.c
+++ src/file_utils.c
@@ -4,47 +4,47 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-FILE* xfopen(const char* __restrict fname,
-             const char* __restrict mode)
+FILE *xfopen(const char *__restrict fname,
+             const char *__restrict mode)
 {
-  FILE* fp;
-#if _MSC_VER
-  errno_t error;
-  if ((error = fopen_s(&fp, fname, mode)) != 0) {
-#else
-  if ((fp = fopen(fname, mode)) == NULL) {
-#endif
+  FILE *fp;
+  printf("fname: %s\n", fname);
+  if ((fp = fopen(fname, mode)) == NULL)
+  {
     fprintf(stderr, "Error occurs\n");
     exit(1);
-  } else {
+  }
+  else
+  {
     return fp;
   }
 }
 
-void xfclose(FILE* fp)
+void xfclose(FILE *fp)
 {
   fclose(fp);
 }
 
-size_t get_file_size(FILE* fp)
+size_t get_file_size(FILE *fp)
 {
   struct stat stbuf;
   const int fd = fileno(fp);
-  if (fstat(fd, &stbuf) == -1) {
+  if (fstat(fd, &stbuf) == -1)
+  {
     perror("Error occurs at get_file_size");
     exit(1);
   }
   return stbuf.st_size;
 }
 
-vector_ptr_string* read_lines(FILE* fp)
+vector_ptr_string *read_lines(FILE *fp)
 {
-  string* buffer = new_string();
+  string *buffer = new_string();
   const size_t file_size = get_file_size(fp) + 1; // include '\0'
   resize_noinit_string(buffer, file_size);
   const size_t num_read = fread(string_to_char(buffer), sizeof(char), file_size, fp);
   UNUSED_PARAMETER(num_read);
-  vector_ptr_string* ret = split_string(buffer, "\n");
+  vector_ptr_string *ret = split_string(buffer, "\n");
   delete_string(buffer);
   return ret;
 }
